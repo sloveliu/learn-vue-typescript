0bac95613b57d4657f28004a14c26d49b96f6409
william.liu <sloveliu@gmail.com>
2023-06-03 11:35:29 +0800

    first commit

---
+++ 01/index.ts
@@ -0,0 +1,7 @@
+(() => {
+  function showMsg(person: string[]){
+      return "天龍三兄弟:" + person.join(",");
+  }
+  let user = ["喬峰","段譽","虛竹"];
+  document.body.innerText = showMsg(user);
+})(); // 立即函式的寫法，當看到結尾 () 時表示要立即執行，裡面可以放參數
